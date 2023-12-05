41307936442172975052fdd673e8d595751ee9ee
Julio Shaya <julio.shaya@gmail.com>
2023-12-05 19:18:51 -0300

    Alphatec
    Comienzo Web Page Alphatec

---
+++ Alphatec/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/logo.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>  
+          <img src={reactLogo} className="logo" alt="Alphatec" />
+      </main>  
+      <body>
+          <div>
+            <h1>Página en Construcción</h1>
+          </div>
+     
+      </body>
+   
+    </>
+  )
+}
+
+export default App
